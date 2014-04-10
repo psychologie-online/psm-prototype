4df18840958132118fe8531e9a2751fdd89f38bc
mahish <nezapomenout@gmail.com>
2014-04-10 18:32:07 +0200

    Posen šipkama w/o .get(0)
    http://stackoverflow.com/questions/21957939/0-click-or-just-click

--- js/cesta.js
+++ js/cesta.js
@@ -1,10 +1,10 @@
 // Posun šipkama v cestě
 $(document).keydown(function (event) {
   if (event.which == 37) {
-    $('#prevPage').get(0).click();
+    $('#prevPage')[0].click();
     // console.log ('prev');
   } else if (event.which == 39) {
-    $('#nextPage').get(0).click();
+    $('#nextPage')[0].click();
     // console.log ('next');
   }
 });
